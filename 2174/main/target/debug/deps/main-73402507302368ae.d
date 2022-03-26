661b2cbad53c03e6de62dd96a4a75de0e672e5a9
Youngho Cho <dominoyh5@gmail.com>
2022-03-26 22:55:21 +0900

    apply gitignore

--- 2174/main/target/debug/deps/main-73402507302368ae.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/side-study/acmicpc-net/2174/main/target/debug/deps/main-73402507302368ae: src/main.rs
-
-/home/ubuntu/side-study/acmicpc-net/2174/main/target/debug/deps/main-73402507302368ae.d: src/main.rs
-
-src/main.rs:
--- 2174/main/target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/side-study/acmicpc-net/2174/main/target/debug/main: /home/ubuntu/side-study/acmicpc-net/2174/main/src/main.rs
