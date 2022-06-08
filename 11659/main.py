3be3d61ef6fb9a550e89a80a27e6acb1476e80e4
Youngho Cho <dominoyh5@gmail.com>
2022-06-08 14:44:29 +0000

    backup 11659

--- 11659/main.py
+++ 11659/main.py
@@ -1,7 +1,23 @@
 import sys
 n, m = map(int, sys.stdin.readline().split())
 ns = list(map(int, sys.stdin.readline().split()))
-
+ms = []
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    print(sum(ns[a-1:b]))
+    ms.append([a-b, a, b])
+ms.sort()
+
+def search(x, y):
+    for i in range(m):
+        if x <= ms[i][1] and y <= ms[i][2]:
+            try:
+                ms[i][3]
+                return ...
+            except:
+                pass
+    return sum(...)
+
+
+def solution():
+    for i in range(m):
+        ms[i].append(search(ms[i][1], ms[i][2]))
