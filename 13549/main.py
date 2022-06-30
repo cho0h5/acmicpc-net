5e3ab7597ca177fec2ac6d215fc25c154dfd4165
Youngho Cho <dominoyh5@gmail.com>
2022-06-30 14:10:44 +0000

    13549 c

--- 13549/main.py
+++ 13549/main.py
@@ -2,14 +2,29 @@ s, d = map(int, input().split())
 
 # global
 INF = 1000000
-cost = [INF] * 100001
+cost = [INF] * 100101
+
+# initialize
 cost[s] = 0
 
 # solution
-
-
-
-
+from collections import deque
+def search():
+    q = deque([s-1, s+1, s*2])
+    while q:
+        n = q.popleft()
+        if n > 100010 or n < 0:
+            continue
+        m = 0
+        if n % 2 == 0:
+            m = min(cost[n-1]+1, cost[n+1]+1, cost[n//2])
+        else:
+            m = min(cost[n-1]+1, cost[n+1]+1)
+        if cost[n] > m:
+            cost[n] = m
+            q.extend([n-1, n+1, n*2])
 
 # out
+search()
+# print("debug:", cost[0:40])
 print(cost[d])
