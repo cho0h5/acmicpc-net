dfc55825bcc5a24bfd02eb77683f830caecefdcb
Youngho Cho <dominoyh5@gmail.com>
2023-04-03 20:14:40 +0900

    7576

--- 1043/main.cpp
+++ 1043/main.cpp
@@ -31,34 +31,56 @@ int main() {
     for(int i = 0; i < M; i++) {
 	for(int j = 0; j < party[i].size(); j++) {
 	    for(int k = j + 1; k < party[i].size(); k++) {
-		cout << party[i][j] << ' ' << party[i][k] << '\n';
-		graph[party[j][j]][party[i][k]] = 1;
+		graph[party[i][j]][party[i][k]] = 1;
 		graph[party[i][k]][party[i][j]] = 1;
-    cout << "[check graph]\n";
-    for(int i = 1; i <= N; i++) {
-	for(int j = 1; j <= N; j++) {
-	    cout << graph[i][j] << ' ';
-	}
-	cout << '\n';
-    }
 	    }
 	}
     }
 
     // check
-    cout << "[check party]\n";
-    for(int i = 0; i < M; i++) {
-	for(auto n : party[i]) {
-	    cout << n << ' ';
+//    cout << "[check party]\n";
+//    for(int i = 0; i < M; i++) {
+//	for(auto n : party[i]) {
+//	    cout << n << ' ';
+//	}
+//	cout << '\n';
+//    }
+//    cout << "[check graph]\n";
+//    for(int i = 1; i <= N; i++) {
+//	for(int j = 1; j <= N; j++) {
+//	    cout << graph[i][j] << ' ';
+//	}
+//	cout << '\n';
+//    }
+
+    while(!q.empty()) {
+	int node = q.front();
+	q.pop();
+	if(isVisit[node] == 1) continue;
+	isVisit[node] = 1;
+	for(int i = 1; i <= N; i++) {
+	    if(graph[node][i] == 1)
+		q.push(i);
 	}
-	cout << '\n';
-    }
-    cout << "[check graph]\n";
-    for(int i = 1; i <= N; i++) {
-	for(int j = 1; j <= N; j++) {
-	    cout << graph[i][j] << ' ';
-	}
-	cout << '\n';
     }
 
+//    cout << "[check isVisit]\n";
+//    for(int i = 1; i <= N; i++) {
+//	cout << isVisit[i] << ' ';
+//    }
+//    cout << '\n';
+
+    // result
+    int count = 0;
+    for(int i = 0; i < M; i++) {
+	int flag = 1;
+	for(auto n : party[i]) {
+	    if(isVisit[n] == 1) {
+		flag = 0;
+		break;
+	    }
+	}
+	count +=  flag;
+    }
+    cout << count;
 }
--- 7576/main.cpp
+++ 7576/main.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
+#include <queue>
+#include <tuple>
 
 using namespace std;
+
+int table[1000][1000];
+int M, N;
+
+int check(int x, int y, int s) {
+    if(x < 0 || x >= N) return 0;
+    if(y < 0 || y >= M) return 0;
+    if(table[x][y] == -1) return 0;
+
+    if(table[x][y] == 0) return 1;
+    return table[x][y] > s;
+}
+
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    queue<tuple<int, int, int> > q;
+
+    // init
+    cin >> M >> N;
+    for(int i = 0; i < N; i++)
+	for(int j = 0; j < M; j++) {
+	    cin >> table[i][j];
+	    if(table[i][j] == 1)
+		q.push(make_tuple(i, j, 1));
+	}
+
+    // solution
+    while(!q.empty()) {
+	int x, y, s;
+	tie(x, y, s) = q.front();
+	q.pop();
+
+	if(check(x-1, y, s+1)) {
+	    table[x-1][y] = s+1;
+	    q.push(make_tuple(x-1, y, s+1));
+	}
+	if(check(x+1, y, s+1)) {
+	    table[x+1][y] = s+1;
+	    q.push(make_tuple(x+1, y, s+1));
+	}
+	if(check(x, y-1, s+1)) {
+	    table[x][y-1] = s+1;
+	    q.push(make_tuple(x, y-1, s+1));
+	}
+	if(check(x, y+1, s+1)) {
+	    table[x][y+1] = s+1;
+	    q.push(make_tuple(x, y+1, s+1));
+	}
+    }
+
+    int max = 0;
+    for(int i = 0; i < N; i++) {
+	for(int j = 0; j < M; j++) {
+	    if(table[i][j] == 0) {
+		cout << "-1\n";
+		return 0;
+	    }
+	    if(table[i][j] > max) max = table[i][j];
+	}
+    }
+    cout << --max;
 }
+
