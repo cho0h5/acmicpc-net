7fa8af6b0f8b94dd910f186f1dbf65e7be7a3874
Youngho Cho <dominoyh5@gmail.com>
2023-04-05 16:04:55 +0900

    1238

--- 1238/main.cpp
+++ 1238/main.cpp
@@ -10,27 +10,6 @@ int N, M, X;
 int isVisit[1001];
 vector<pair<int, int> > graph[1001];
 
-int search(int S, int D) {
-    for(int i = 1; i <= N; i++) isVisit[i] = 1000;
-    queue<pair<int, int> > q;
-    q.push(make_pair(S, 0));
-
-    while(!q.empty()) {
-	int node, cost;
-	tie(node, cost) = q.front();
-	q.pop();
-	if(isVisit[node] <= cost) continue;
-	isVisit[node] = cost;
-	for(int i = 0; i < graph[node].size(); i++) {
-	    int n2, n2cost;
-	    tie(n2, n2cost) = graph[node][i];
-	    q.push(make_pair(n2, cost + n2cost));
-	}
-    }
-
-    return isVisit[D];
-}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -42,11 +21,10 @@ int main() {
 	graph[n1].push_back(make_pair(n2, cost));
     }
 
-    int max = 0;
     for(int i = 1; i <= N; i++) {
-	if(i == X) continue;
-	int cost = search(i, X) + search(X, i);
-	if(cost > max) max = cost;
+	for(int j = 0; j < graph[i].size(); j++) {
+	    cout << i << ' ' << graph[i][j].first << ' ' << graph[i][j].second << '\n';
+	}
     }
-    cout << max;
+
 }
