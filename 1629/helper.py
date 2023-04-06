37738d3ffc51fa8bad93bdb00dd0b88b87d6aa4c
Youngho Cho <dominoyh5@gmail.com>
2023-04-06 14:34:01 +0900

    1629

---
+++ 1629/helper.py
@@ -0,0 +1,15 @@
+def solution(A, B, C):
+    return A ** B % C
+
+
+
+
+
+#for i in range(2, 10):
+#
+#    for j in range(1, 20):
+#        print(solution(i, j, 11), end=' ')
+#
+#    print("")
+
+print(solution(23456, 9876, 192837))
--- 1629/main.cpp
+++ 1629/main.cpp
@@ -5,11 +5,25 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int A, B, C;
-    int acc;
+    long long A, B, C;
+    long long acc = 1;
     cin >> A >> B >> C;
-    while(B--) {
-	acc = acc * A % C;
+
+    // 1
+    A = A % C;
+    // cout << "[B & 0x01] " << (B & 0x01) << '\n';
+    if(B & 0x01) acc = acc * A % C;
+    // cout << "[A] " << A << '\n';
+    // cout << "[acc] " << acc << '\n';
+    B = B >> 1;
+
+    // 2, 4, 8,...
+    for(; B > 0; B = B >> 1) {
+	A = A * A % C;
+	// cout << "[B & 0x01] " << (B & 0x01) << '\n';
+	if(B & 0x01) acc = acc * A % C;
+	// cout << "[A] " << A << '\n';
+	// cout << "[acc] " << acc << '\n';
     }
     cout << acc;
 }
