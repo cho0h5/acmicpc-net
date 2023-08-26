678caa7b282151aa1144948842eabf7268c310d8
Youngho Cho <dominoyh5@gmail.com>
2023-08-26 11:40:02 +0900

    11003

--- 11003/main.cpp
+++ 11003/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
-#include <deque>
+#include <queue>
 
 using namespace std;
 
 int main() {
-    deque<int> dq;
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    queue<int> dq;
+    priority_queue<int> mq;
+    priority_queue<int> sq;
+
     int n, l;
     cin >> n >> l;
 
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        dq.push_back(x);
+        dq.push(x);
+        mq.push(-x);
         if (dq.size() > l) {
-            dq.pop_front();
+            int target = dq.front();
+            dq.pop();
+            if (mq.top() == -target) {
+                mq.pop();
+                while (!sq.empty() && mq.top() == sq.top()) {
+                    mq.pop();
+                    sq.pop();
+                }
+            } else {
+                sq.push(-target);
+            }
         }
+        cout << -mq.top() << ' ';
     }
+    cout << '\n';
 }
