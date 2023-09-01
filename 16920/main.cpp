fe7eff64cae794a145f342cf43513d80a8dca89f
조영호 <younghoc@joyeonghos-MacBook-Air-2.local>
2023-09-01 14:54:18 +0900

    16920

--- 16920/main.cpp
+++ 16920/main.cpp
@@ -9,8 +9,8 @@ int dx[] = {1, 0, -1, 0};
 int dy[] = {0, -1, 0, 1};
 
 char board[1000][1001];
-int vis[1000][1000];
 int s[9];
+int r[9];
 
 int h, w, p;
 
@@ -43,16 +43,17 @@ int main() {
         for (int j = 0; j < w; j++) {
             if ('1' <= board[i][j] && board[i][j] <= '9') {
                 q[board[i][j] - '1'].push(make_pair(i, j));
+                r[board[i][j] - '1']++;
             }
         }
     }
-    
-    printb();
+    // printb();
 
     while (!empty()) {
         for (int i = 0; i < p; i++) {
             for (int j = 0; j < s[i]; j++) {
                 int s = q[i].size();
+                if (s == 0) break;
                 for (int k = 0; k < s; k++) {
                     pair<int, int> cur = q[i].front(); q[i].pop();
 
@@ -61,15 +62,19 @@ int main() {
                         int ny = cur.second + dy[l];
 
                         if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                        if (vis[nx][ny] != 0 || board[nx][ny] != '.') continue;
+                        if (board[nx][ny] != '.') continue;
 
-                        vis[nx][ny] = 1;
                         board[nx][ny] = i + '1';
+                        r[i]++;
                         q[i].push(make_pair(nx, ny));
                     }
                 }
             }
         }
     }
-    printb();
+    // printb();
+    for(int i = 0; i < p; i++) {
+        cout << r[i] << ' ';
+    }
+    cout << '\n';
 }
