3fce2b9d7a813001cfdcbe97cd989cea0088ac9e
조영호 <younghoc@joyeonghos-MacBook-Air-2.local>
2023-09-03 11:37:47 +0900

    15657 ok

--- 15657/main.cpp
+++ 15657/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int ns[7];
+int p[7];
+int n, t;
+int cnt;
+
+void perm(int s) {
+    if (s == t) {
+        for (int i = 0; i < t; i++) {
+            cout << ns[p[i]] << ' ';
+        }
+        cout << '\n';
+        return ;
+    }
+
+    if (s == 0) {
+        for (int i = 0; i < n; i++) {
+            p[s] = i;
+            perm(s + 1);
+        }
+    } else {
+        for (int i = p[s - 1]; i < n; i++) {
+            p[s] = i;
+            perm(s + 1);
+        }
+    }
+}
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin >> n >> t;
+    for (int i = 0; i < n; i++) cin >> ns[i];
+    sort(ns, ns + n);
+
+    perm(0);
 }
