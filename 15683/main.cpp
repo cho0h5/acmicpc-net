07bed3e64d0e89f353a3b90fe045ed557eca7657
Youngho Cho <dominoyh5@gmail.com>
2023-09-09 08:59:04 +0900

    15683 keep

---
+++ 15683/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int board[8][8];
+
+void printb() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << board[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    printb();
+}
