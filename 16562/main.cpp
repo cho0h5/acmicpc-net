e834c693fd7d013e210bafa00d8fe31313f4e2dc
Youngho Cho <dominoyh5@gmail.com>
2024-02-13 12:37:26 +0900

    16562 postpone

---
+++ 16562/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int n, m, k;
+int a;
+int v, w;
+int cost[10001];
+
+using namespace std;
+
+int main() {
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; i++)
+		cin >> cost[i];
+
+	for (int i = 0; i < m; i++) {
+		cin >> v >> w;
+	}
+}
