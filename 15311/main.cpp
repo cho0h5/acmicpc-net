0aea8f4746e9dffa72c4f787438ef5653ef98ff9
Youngho Cho <dominoyh5@gmail.com>
2024-02-17 22:24:53 +0900

    15311

---
+++ 15311/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "2000\n";
+	for (int i = 0; i < 1000; i++)
+		cout << "1000 ";
+	for (int i = 0; i < 1000; i++)
+		cout << "1 ";
+	cout << '\n';
+}
