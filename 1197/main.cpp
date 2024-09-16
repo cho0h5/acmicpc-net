1f8dec467e86a12d6a71bed2e696a65e98db757f
Youngho Cho <dominoyh5@gmail.com>
2024-09-16 20:08:36 +0900

    init: 1197

--- 1197/main.cpp
+++ 1197/main.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
-#include <utility>
-#include <tuple>
-#include <vector>
-#include <stack>
-#include <algorithm>
-#include <map>
-#include <set>
 
 using namespace std;
 
@@ -15,40 +8,6 @@ void fastio() {
 	cout.tie(NULL);
 }
 
-int v, e;
-vector<tuple<int, int, int>> edges;
-int table[10001];
-
-int parent(int node) {
-	if (node == table[node])
-		return node;
-	return parent(table[node]);
-}
-
 int main() {
 	fastio();
-
-	cin >> v >> e;
-	for (int i = 1; i <= v; i++) {
-		table[i] = i;
-	}
-	while (e--) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		edges.push_back(make_tuple(c, a, b));
-	}
-	sort(edges.begin(), edges.end());
-
-	int total_cost = 0;
-	for (auto it = edges.begin(); it != edges.end(); it++) {
-		auto [cost, a, b] = *it;
-		int pa = parent(a);
-		int pb = parent(b);
-		if (pa != pb) {
-			total_cost += cost;
-			if (pa < pb) table[pb] = pa;
-			else table[pa] = pb;
-		}
-	}
-	cout << total_cost << '\n';
 }
