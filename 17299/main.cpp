eb0eaeffb980dd4f97874b065f2f001d7cca95f5
Youngho Cho <dominoyh5@gmail.com>
2024-10-24 12:38:57 +0900

    17299

--- 17299/main.cpp
+++ 17299/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <queue>
-#include <map>
+#include <stack>
 
 using namespace std;
 
 int n;
 int ns[1000000];
 int answer[1000000];
-priority_queue<pair<int, int>> pq;
-map<int, int> m;
+int m[1000001];
+stack<pair<int, int>> s;
 
 void sol() {
 	for (int i = n - 1; i >= 0; i--) {
-		if (pq.empty()) {
+		const int Ai = ns[i];
+
+		while (s.size() && s.top().second <= m[Ai]) {
+			s.pop();
+		}
+
+		if (s.empty()) {
 			answer[i] = -1;
 		} else {
-			answer[i] = pq.top().second;
+			answer[i] = s.top().first;
 		}
-		pq.push({m[ns[n - 1]], ns[n - 1]});
+		s.push({Ai, m[Ai]});
 	}
 }
 
@@ -29,6 +34,10 @@ void print_answer() {
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> ns[i];
