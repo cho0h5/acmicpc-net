00508b13ba5f219bc1f96e1856a76b35b615eaea
Youngho Cho <dominoyh5@gmail.com>
2024-11-17 16:49:51 +0900

    5214: timeout

--- 5214/main.cpp
+++ 5214/main.cpp
@@ -11,34 +11,33 @@ vector<unordered_set<int>> hs;
 int sol() {
     unordered_set<int> vst;
     priority_queue<pair<int, int>> pq;
-    pq.emplace(-1, 1);
+    for (int i = 0; i < hs.size(); i++) {
+        if (hs[i].find(1) != hs[i].end()) {
+            pq.emplace(-2, i);
+        }
+    }
 
     while (pq.size()) {
-        auto [curr_cost, curr_index] = pq.top();
-        curr_cost = -curr_cost;
-        const int next_cost = curr_cost + 1;
+        auto [curr_cost, curr_hiper] = pq.top();
+        const int next_cost = curr_cost - 1;
         pq.pop();
-        // cout << "pop: " << curr_index << " - " << curr_cost << '\n';
 
-        if (curr_index == n) {
-            return curr_cost;
+        if (hs[curr_hiper].find(n) != hs[curr_hiper].end()) {
+            return -curr_cost;
         }
 
-        for (auto &h: hs) {
-            if (h.find(curr_index) == h.end()) {
-                continue;
-            }
-
-            for (auto &next_index: h) {
-                if (vst.find(next_index) == vst.end()) {
-                    vst.insert(next_index);
-//                    cout << "push: " << curr_index << " - " << curr_cost << ", "
-//                        << next_index << " - " << next_cost << '\n';
-                    pq.emplace(-next_cost, next_index);
+        for (int i = 0; i < hs.size(); i++) {
+            if (hs[i].empty()) continue;
+            if (vst.find(i) == vst.end()) {
+                for (auto &next_index: hs[curr_hiper]) {
+                    if (hs[i].find(next_index) != hs[i].end()) {
+                        vst.insert(i);
+                        pq.push({next_cost, i});
+                    }
                 }
             }
-            h.clear();
         }
+        hs[curr_hiper].clear();
     }
 
     return -1;
@@ -59,8 +58,8 @@ void input() {
 
 int main() {
     ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     input();
     cout << sol() << '\n';
