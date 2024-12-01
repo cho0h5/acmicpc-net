ce4d1e110c4af137337553dc38e75c4f6f8deaf4
Youngho Cho <dominoyh5@gmail.com>
2024-12-01 13:26:56 +0900

    14391

--- 14391/main.cpp
+++ 14391/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int paper[4][4];
+string paper[4];
 
 int get(const int i_start, const int j_start,
         const int i_end, const int j_end) {
@@ -12,13 +12,19 @@ int get(const int i_start, const int j_start,
     if (i_size >= 2) {
         int temp_cost = 0;
         for (int i = i_start; i < i_end; i++) {
-            temp_cost += paper[i][j_start];
+            temp_cost += paper[i][j_start] - '0';
+            if (i != i_end - 1) {
+                temp_cost *= 10;
+            }
         }
         return temp_cost;
     } else {
         int temp_cost = 0;
         for (int j = j_start; j < j_end; j++) {
-            temp_cost += paper[i_start][j];
+            temp_cost += paper[i_start][j] - '0';
+            if (j != j_end - 1) {
+                temp_cost *= 10;
+            }
         }
         return temp_cost;
     }
@@ -39,6 +45,7 @@ int dfs(const int i_start, const int j_start,
         temp_cost += get(i_start + i, j_start, i_start + i + 1, j_end);
         temp_cost += dfs(i_start + i + 1, j_start, i_end, j_end);
         max_cost = max(max_cost, temp_cost);
+        // cout << "temp_cost: " << temp_cost << '\n';
     }
     for (int j = 0; j < j_size; j++) {
         int temp_cost = 0;
@@ -46,6 +53,7 @@ int dfs(const int i_start, const int j_start,
         temp_cost += get(i_start, j_start + j, i_end, j_start + j + 1);
         temp_cost += dfs(i_start, j_start + j + 1, i_end, j_end);
         max_cost = max(max_cost, temp_cost);
+        // cout << "temp_cost: " << temp_cost << '\n';
     }
     return max_cost;
 }
@@ -54,9 +62,8 @@ int main() {
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> paper[i][j];
-        }
+        cin >> paper[i];
     }
+
     cout << dfs(0, 0, n, m) << '\n';
 }
