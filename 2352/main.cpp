199084327406c031975d4ba2e7a712834e61ff45
Youngho Cho <dominoyh5@gmail.com>
2025-06-06 14:08:47 +0900

    2352

---
+++ 2352/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[40000];
+int lis[40000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    lis[0] = arr[0];
+    int len = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (lis[len - 1] < arr[i]) {
+            lis[len] = arr[i];
+            len += 1;
+            continue;
+        }
+
+        auto it = lower_bound(lis, lis + len, arr[i]);
+        // cout << "it: " << *it << '\n';
+
+        if (it == lis + len) {
+            *it = arr[i];
+            len += 1;
+        } else {
+            if (*it > arr[i]) {
+                *it = arr[i];
+            }
+        }
+    }
+
+//     for (int i = 0; i < len; i++) {
+//         cout << lis[i] << ' ';
+//     }
+//     cout << '\n';
+    
+    cout << len << '\n';
+}
