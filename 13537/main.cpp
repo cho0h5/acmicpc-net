0062f2eade537f35bb5cf4ab2e327c2446b708a0
Youngho Cho <dominoyh5@gmail.com>
2025-06-08 15:13:01 +0900

    13537

--- 13537/main.cpp
+++ 13537/main.cpp
@@ -1,98 +1,46 @@
 #include <iostream>
-
-#define MAXIMUM 1000000000
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-// stuructures
-
-struct Node {
-    int state;
-    Node *children[4];
-};
-
-// global variables
-
-Node nodes[12000000];
-int nodes_len;
-
 int n, m;
-Node *root;
+vector<int> tree[400004];
+int arr[100001];
 
-// helper functions
-
-Node *nodes_new() {
-    const int i = nodes_len++;
-    return &nodes[i];
-}
-
-void set(Node *node, int x_s, int x_e, int y_s, int y_e, int i, int j) {
-    if (i < x_s || x_e < i || j < y_s || y_e < j) {
+void build(int node, int s, int e) {
+    if (s == e) {
+        tree[node].push_back(arr[s]);
         return;
     }
 
-    node->state += 1;
+    int mid = (s + e) >> 1;
+    int left = node * 2;
+    int right = left + 1;
 
-    if (y_s == y_e) {
-        return;
-    }
+    build(left, s, mid);
+    build(right, mid + 1, e);
 
-    if (node->children[0] == NULL) {
-        for (int i = 0; i < 4; i++) {
-            node->children[i] = nodes_new();
-        }
-    }
-
-    const int x_mid = (x_s + x_e) >> 1;
-    const int y_mid = (y_s + y_e) >> 1;
-    set(node->children[0], x_s, x_mid,      y_s, y_mid,     i, j);
-    set(node->children[1], x_mid + 1, x_e,  y_s, y_mid,     i, j);
-    set(node->children[2], x_s, x_mid,      y_mid + 1, y_e, i, j);
-    set(node->children[3], x_mid + 1, x_e,  y_mid + 1, y_e, i, j);
+    merge(tree[left].begin(), tree[left].end(), tree[right].begin(), tree[right].end(), back_inserter(tree[node]));
 }
 
-int get(Node *node, int x_s, int x_e, int y_s, int y_e, int x_i, int x_j, int y_i, int y_j) {
-    if (x_j < x_s || x_e < x_i || y_j < y_s || y_e < y_i) {
+int get(int node, int s, int e, int i, int j, int x) {
+    if (j < s || e < i) {
         return 0;
     }
 
-    if (x_i <= x_s && x_e <= x_j && y_i <= y_s && y_e <= y_j) {
-        return node->state;
+    if (i <= s && e <= j) {
+        return tree[node].end() - upper_bound(tree[node].begin(), tree[node].end(), x);
     }
 
-    if (node->children[0] == NULL) {
-        return 0;
-    }
-
-    const int x_mid = (x_s + x_e) >> 1;
-    const int y_mid = (y_s + y_e) >> 1;
+    int mid = (s + e) >> 1;
+    int left = node * 2;
+    int right = left + 1;
     int sumation = 0;
-    sumation += get(node->children[0], x_s, x_mid,      y_s, y_mid,     x_i, x_j, y_i, y_j);
-    sumation += get(node->children[1], x_mid + 1, x_e,  y_s, y_mid,     x_i, x_j, y_i, y_j);
-    sumation += get(node->children[2], x_s, x_mid,      y_mid + 1, y_e, x_i, x_j, y_i, y_j);
-    sumation += get(node->children[3], x_mid + 1, x_e,  y_mid + 1, y_e, x_i, x_j, y_i, y_j);
+    sumation += get(left, s, mid, i, j, x);
+    sumation += get(right, mid + 1, e, i, j, x);
     return sumation;
 }
-
-void print(Node *node, int x_s, int x_e, int y_s, int y_e, int step) {
-    for (int i = 0; i < step; i++) {
-        cout << "  ";
-    }
-    cout << node->state << " (" << x_s << ", " << x_e << ", " << y_s << ", " << y_e << ")\n";
-
-    if (node->children[0] == NULL) {
-        return;
-    }
-
-    const int x_mid = (x_s + x_e) >> 1;
-    const int y_mid = (y_s + y_e) >> 1;
-    print(node->children[0], x_s, x_mid,      y_s, y_mid,     step + 1);
-    print(node->children[1], x_mid + 1, x_e,  y_s, y_mid,     step + 1);
-    print(node->children[2], x_s, x_mid,      y_mid + 1, y_e, step + 1);
-    print(node->children[3], x_mid + 1, x_e,  y_mid + 1, y_e, step + 1);
-}
-
-// main
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -100,20 +48,16 @@ int main() {
     cout.tie(NULL);
 
     cin >> n;
-    root = nodes_new();
     for (int i = 1; i <= n; i++) {
-        int a;
-        cin >> a;
-
-        set(root, 1, n, 1, MAXIMUM, i, a);
+        cin >> arr[i];
     }
 
-    // print(root, 1, n, 1, MAXIMUM, 0);
+    build(1, 1, n);
 
     cin >> m;
     for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        cout << get(root, 1, n, 1, MAXIMUM, a, b, c + 1, MAXIMUM) << '\n';
+        cout << get(1, 1, n, a, b, c) << '\n';
     }
 }
