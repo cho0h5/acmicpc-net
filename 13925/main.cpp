c0299a206df89683323a443ee84608578a4b8ad1
Youngho Cho <dominoyh5@gmail.com>
2025-06-09 17:59:24 +0900

    13925: fail

--- 13925/main.cpp
+++ 13925/main.cpp
@@ -8,17 +8,145 @@ int lazy_add[400004];
 int lazy_mul[400004];
 int lazy_ovr[400004];
 
+void propagate_add(int node, int s, int e);
+void propagate_mul(int node, int s, int e);
+void propagate_ovr(int node, int s, int e);
+
+void propagate_add(int node, int s, int e) {
+    int mid = (s + e) >> 1;
+    int left = node >> 1;
+    int right = left + 1;
+
+    if (lazy_add[node] == 0) {
+        return;
+    }
+
+    propagate_mul(left, s, mid);
+    propagate_ovr(left, s, mid);
+    propagate_mul(right, mid + 1, e);
+    propagate_ovr(right, mid + 1, e);
+
+    if (s != e) {
+        lazy_add[left] = lazy_add[node];
+        lazy_add[right] = lazy_add[node];
+    }
+
+    state[node] += lazy_add[node];
+    lazy_add[node] = 0;
+
+    return;
+}
+
+void propagate_mul(int node, int s, int e) {
+    int mid = (s + e) >> 1;
+    int left = node >> 1;
+    int right = left + 1;
+
+    if (lazy_mul[node] == 0) {
+        return;
+    }
+
+    propagate_add(left, s, mid);
+    propagate_ovr(left, s, mid);
+    propagate_add(right, mid + 1, e);
+    propagate_ovr(right, mid + 1, e);
+
+    if (s != e) {
+        lazy_mul[left] = lazy_mul[node];
+        lazy_mul[right] = lazy_mul[node];
+    }
+
+    state[node] *= lazy_mul[node];
+    lazy_mul[node] = 0;
+
+    return;
+}
+
+void propagate_ovr(int node, int s, int e) {
+    int mid = (s + e) >> 1;
+    int left = node >> 1;
+    int right = left + 1;
+
+    if (lazy_ovr[node] == 0) {
+        return;
+    }
+
+    propagate_add(left, s, mid);
+    propagate_mul(left, s, mid);
+    propagate_add(right, mid + 1, e);
+    propagate_mul(right, mid + 1, e);
+
+    if (s != e) {
+        lazy_ovr[left] = lazy_ovr[node];
+        lazy_ovr[right] = lazy_ovr[node];
+    }
+
+    state[node] = lazy_ovr[node];
+    lazy_ovr[node] = 0;
+
+    return;
+}
+
 void update_add(int node, int s, int e, int i, int j, int x) {
+    if (j < s || e < i) {
+        return;
+    }
+
+    propagate_add(node, s, e);
+    propagate_mul(node, s, e);
+    propagate_ovr(node, s, e);
+
+    if (i <= s && e <= j) {
+        lazy_add[node] = x;
+        return;
+    }
+
+    state[node] += x;
+
+    int mid = (s + e) >> 1;
+    update_add(node * 2, s, mid, i, j, x);
+    update_add(node * 2 + 1, mid + 1, e, i, j, x);
 }
 
 void update_mul(int node, int s, int e, int i, int j, int x) {
+    if (j < s || e < i) {
+        return;
+    }
+
+    propagate_add(node, s, e);
+    propagate_mul(node, s, e);
+    propagate_ovr(node, s, e);
+
+    if (i <= s && e <= j) {
+        lazy_mul[node] = x;
+        return;
+    }
+
+    state[node] *= x;
+
+    int mid = (s + e) >> 1;
+    update_mul(node * 2, s, mid, i, j, x);
+    update_mul(node * 2 + 1, mid + 1, e, i, j, x);
 }
 
 void update_ovr(int node, int s, int e, int i, int j, int x) {
 }
 
 int retreive(int node, int s, int e, int i, int j) {
-    return -1;
+    if (j < s || e < i) {
+        return 0;
+    }
+
+    propagate_add(node, s, e);
+    propagate_mul(node, s, e);
+    propagate_ovr(node, s, e);
+
+    if (i <= s && e <= j) {
+        return state[node];
+    }
+
+    int mid = (s + e) >> 1;
+    return retreive(node * 2, s, mid, i, j) + retreive(node * 2 + 1, mid + 1, e, i, j);
 }
 
 int main() {
@@ -28,7 +156,9 @@ int main() {
 
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        cin >> state[i];
+        int x;
+        cin >> x;
+        update_add(1, 1, n, i, i, x);
     }
 
     cin >> m;
