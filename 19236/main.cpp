51387aaaa363165ba404381581b6322e7730787a
Youngho Cho <dominoyh5@gmail.com>
2025-07-18 14:52:16 +0900

    19236: reimpl wrong

--- 19236/main.cpp
+++ 19236/main.cpp
@@ -3,72 +3,42 @@
 
 using namespace std;
 
-int map_name[4][4];
-int map_fishes[17][2];
-int map_dir[4][4];
+struct State {
+    int name[4][4];
+    int dir[4][4];
 
-int dup_name[4][4];
-int dup_fishes[17][2];
-int dup_dir[4][4];
+    int shark[2];
+    int fishes[17][2];
 
-int arr[16];
+    int score;
+};
 
 int maximum = -1;
 
 const int dx[9] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
 const int dy[9] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
 
-void swap_fish(const int a, const int b) {
-    const int ax = dup_fishes[a][0];
-    const int ay = dup_fishes[a][1];
-    const int bx = dup_fishes[b][0];
-    const int by = dup_fishes[b][1];
-
-    dup_fishes[a][0] = bx;
-    dup_fishes[a][1] = by;
-    dup_fishes[b][0] = ax;
-    dup_fishes[b][1] = ay;
-
-    // dup_name[ax][ay] = b + 1;
-    // dup_name[bx][by] = a + 1;
-
-    // printf("%d, %d\n", ax, ay);
-    const int tmp = dup_name[ax][ay];
-    dup_name[ax][ay] = dup_name[bx][by];
-    dup_name[bx][by] = tmp;
-
-    const int temp = dup_dir[ax][ay];
-    dup_dir[ax][ay] = dup_dir[bx][by];
-    dup_dir[bx][by] = temp;
-}
-
-void move_fishes(const int sx, const int sy) {
+void move_fishes(State &state) {
     for (int i = 0; i < 16; i++) {
-        if (dup_fishes[i][0] == -1) {
+        if (state.fishes[i][0] == -1) {
             continue;
         }
 
-        const int x = dup_fishes[i][0];
-        const int y = dup_fishes[i][1];
+        const int cx = state.fishes[i][0];
+        const int cy = state.fishes[i][1];
         int nx = -1;
         int ny = -1;
-        for (int i = 0; i < 8; i++) {
-            const int dir = dup_dir[x][y];
-            nx = x + dx[dir];
-            ny = y + dy[dir];
 
-            if (nx == sx && ny == sy) {
-                dup_dir[x][y] += 1;
-                if (dup_dir[x][y] == 9) {
-                    dup_dir[x][y] = 1;
-                }
-                continue;
-            }
+        // Redirect
+        for (int j = 0; j < 8; j++) {
+            const int cd = state.dir[cx][cy];
+            nx = cx + dx[cd];
+            ny = cy + dy[cd];
 
             if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) {
-                dup_dir[x][y] += 1;
-                if (dup_dir[x][y] == 9) {
-                    dup_dir[x][y] = 1;
+                state.dir[cx][cy] += 1;
+                if (state.dir[cx][cy] >= 9) {
+                    state.dir[cx][cy] = 1;
                 }
                 continue;
             }
@@ -76,81 +46,97 @@ void move_fishes(const int sx, const int sy) {
             break;
         }
 
-        if (dup_name[nx][ny] != 0) {
-            // printf("dup_name: %d\n", dup_name[nx][ny]);
-            swap_fish(dup_name[nx][ny] - 1, i);
-        } else {
-            dup_fishes[i][0] = nx;
-            dup_fishes[i][1] = ny;
+        if (nx == state.shark[0] && ny == state.shark[1]) { // If there is a shark, stop
+            continue;
+        } else if (state.name[nx][ny] != 0) {  // If there is fish, switch
+            const int other_fish_i = state.name[nx][ny] - 1;
 
-            dup_name[nx][ny] = dup_name[x][y];
-            dup_name[x][y] = 0;
+            state.name[nx][ny] = i + 1;
+            state.name[cx][cy] = other_fish_i + 1;
 
-            dup_dir[nx][ny] = dup_dir[x][y];
-            dup_dir[x][y] = 0;
+            const int tmp1 = state.dir[nx][ny];
+            state.dir[nx][ny] = state.dir[cx][cy];
+            state.dir[cx][cy] = tmp1;
+
+            state.fishes[i][0] = nx;
+            state.fishes[i][1] = ny;
+            state.fishes[other_fish_i][0] = cx;
+            state.fishes[other_fish_i][1] = cy;
+        } else {    // else, just move
+            state.name[nx][ny] = state.name[cx][cy];
+            state.name[cx][cy] = 0;
+
+            state.dir[nx][ny] = state.dir[cx][cy];
+            state.dir[cx][cy] = 0;
+            
+            state.fishes[i][0] = nx;
+            state.fishes[i][1] = ny;
         }
     }
 }
 
-int sol() {
-    // printf("---- new game ----\n");
-    memcpy(dup_name, map_name, sizeof(dup_name));
-    memcpy(dup_fishes, map_fishes, sizeof(dup_fishes));
-    memcpy(dup_dir, map_dir, sizeof(dup_dir));
-
-    // Put shark
-    int x = 0;
-    int y = 0;
-    int score = 0;
-    int step = 0;
-
-    while (1) {
-        if (step >= 15) {
-            break;
+void print_state(const State &state, const int step) {
+    printf("step: %d\n", step);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%2d ", state.name[i][j]);
         }
-
-        if (x < 0 || x >= 4 || y < 0 || y >= 4) {
-            break;
-        }
-
-        if (dup_name[x][y] == 0) {
-            break;
-        }
-
-        const int target = dup_name[x][y];
-        // printf("ate: %d\n", target);
-        dup_name[x][y] = 0;
-        score += target;
-        dup_fishes[target - 1][0] = -1;
-        dup_fishes[target - 1][1] = -1;
-
-        int nx = x + dx[dup_dir[x][y]] * arr[step];
-        int ny = y + dy[dup_dir[x][y]] * arr[step];
-        dup_dir[x][y] = 0;
-        x = nx;
-        y = ny;
-        step += 1;
-
-        move_fishes(x, y);
+        printf("\n");
     }
-
-    return score;
+    printf("shark: (%d, %d)\n", state.shark[0], state.shark[1]);
+    printf("score: %d\n", state.score);
+    printf("\n");
 }
 
-void backtracking(int step) {
-    if (step == 15) {
-        const int score = sol();
-        if (score > maximum) {
-            // printf("new: %d\n", score);
-            maximum = score;
+void backtracking(State state, const int n, const int step) {
+    print_state(state, step);
+
+    // Simul
+    const int scx = state.shark[0];
+    const int scy = state.shark[1];
+    const int scd = state.dir[scx][scy];
+    const int snx = scx + dx[scd] * n;
+    const int sny = scy + dy[scd] * n;
+
+    // Check if it is possible to move
+    if (snx < 0 || snx >= 4 || sny < 0 || sny >= 4) {
+        if (state.score > maximum) {
+            maximum = state.score;
+            printf("maximum: %d\n", maximum);
         }
         return;
     }
 
-    for (int i = 1; i <= 3; i++) {
-        arr[step] = i;
-        backtracking(step + 1);
+    // Check if there is a fish
+    if (state.name[snx][sny] == 0) {
+        if (state.score > maximum) {
+            maximum = state.score;
+            printf("maximum: %d\n", maximum);
+        }
+        return;
     }
+
+    // Shark eat a fish
+    const int fish_i = state.name[snx][sny] - 1;
+    state.score += fish_i + 1;
+    state.name[snx][sny] = 0;
+    // Remove fish
+    state.fishes[fish_i][0] = 0;
+    state.fishes[fish_i][1] = 0;
+    // Move Shark
+    state.shark[0] = snx;
+    state.shark[1] = sny;
+
+    printf("Eat, move\n");
+    print_state(state, step);
+
+    // Move fishes
+    move_fishes(state);
+
+    // Keep going backtracking
+    backtracking(state, 1, step + 1);
+    backtracking(state, 2, step + 1);
+    backtracking(state, 3, step + 1);
 }
 
 int main() {
@@ -158,19 +144,32 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
+    State state;
+    state.shark[0] = 0;
+    state.shark[1] = 0;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            cin >> map_name[i][j];
-            cin >> map_dir[i][j];
-            map_fishes[map_name[i][j]][0] = i;
-            map_fishes[map_name[i][j]][1] = j;
+            cin >> state.name[i][j];
+            cin >> state.dir[i][j];
+            state.fishes[state.name[i][j]][0] = i;
+            state.fishes[state.name[i][j]][1] = j;
         }
     }
+    const int fish_i = state.name[0][0] - 1;
+    state.name[0][0] = 0;
+    state.score = fish_i + 1;
+    state.fishes[fish_i][0] = -1;
+    state.fishes[fish_i][1] = -1;
 
-    for (int i = 1; i <= 3; i++) {
-        arr[0] = i;
-        backtracking(1);
-    }
+    print_state(state, -1);
+    move_fishes(state);
+    print_state(state, -1);
+
+    // Move fishes
+    move_fishes(state);
+    backtracking(state, 1, 0);
+    backtracking(state, 2, 0);
+    backtracking(state, 3, 0);
 
     printf("%d\n", maximum);
 }
