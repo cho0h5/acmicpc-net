2f2ecdb6e57caf5835e63912df020b6aa9634d37
Youngho Cho <dominoyh5@gmail.com>
2025-07-28 12:59:49 +0900

    15576

--- 15576/main.cpp
+++ 15576/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <complex>
+#include <vector>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ vector<cpx> string_to_vec(const string &str, const int size) {
     vector<cpx> vec(size);
 
     for (int i = 0; i < str.size(); i++) {
-        vec[i + (size - str.size())] = str[i] - '0';
+        vec[i] = str[str.size() - i - 1] - '0';
     }
 
     return vec;
@@ -33,24 +34,19 @@ void fft(vector<cpx> &f, const cpx w) {
     vector<cpx> feven(f.size() / 2);
     vector<cpx> fodd(f.size() / 2);
     for (int i = 0; i < f.size() / 2; i++) {
-        feven[i] = f[i * 2 + 1];
-        fodd[i] = f[i * 2];
+        feven[i] = f[i * 2];
+        fodd[i] = f[i * 2 + 1];
     }
 
     fft(feven, w * w);
     fft(fodd, w * w);
 
+    cpx wi(1, 0);
     for (int i = 0; i < f.size() / 2; i++) {
-        f[i] = feven[i] + fodd[i] * w;
-        f[i + f.size() / 2] = feven[i] - fodd[i] * w;
+        f[i] = feven[i] + fodd[i] * wi;
+        f[i + f.size() / 2] = feven[i] - fodd[i] * wi;
+        wi *= w;
     }
-}
-
-void print_cpxs(const vector<cpx> f) {
-    for (int i = 0; i < f.size(); i++) {
-        printf("%d ", (int)round(f[i].real()));
-    }
-    printf("\n");
 }
 
 int main() {
@@ -60,46 +56,46 @@ int main() {
 
     string a, b;
     cin >> a >> b;
+    if (a == "0" || b == "0") {
+        printf("0\n");
+        return 0;
+    }
 
     const double pi = acos(-1);
     const cpx j(0.0, 1.0);
     const int size = upper_2th(a.size() + b.size());
     const cpx w = exp(2.0 / size * pi * j);
-    printf("w: %f + %fi\n", w.real(), w.imag());
     vector<cpx> f = string_to_vec(a, size);
     vector<cpx> g = string_to_vec(b, size);
 
-    printf("f : ");
-    print_cpxs(f);
-    printf("g : ");
-    print_cpxs(g);
-    printf("\n");
-
     fft(f, w);
     fft(g, w);
-
-    printf("f': ");
-    print_cpxs(f);
-    printf("g': ");
-    print_cpxs(g);
-    printf("\n");
 
     vector<cpx> h(size);
     for (int i = 0; i < size; i++) {
         h[i] = f[i] * g[i];
     }
 
-    printf("h': ");
-    print_cpxs(h);
-
     fft(h, conj(w));
 
+    vector<int> result(size + 1);
     for (int i = 0; i < size; i++) {
-        h[i] /= size;
+        result[i] = round(h[i].real() / size);
     }
 
-    printf("h : ");
-    print_cpxs(h);
+    for (int i = 0; i < size; i++) {
+        if (result[i] >= 10) {
+            result[i + 1] += result[i] / 10;
+            result[i] %= 10;
+        }
+    }
+
+    int i = size - 1;
+    for (; i >= 0 && result[i] == 0; i--);
+    for (; i >= 0; i--) {
+        printf("%d", result[i]);
+    }
+    printf("\n");
 
     return 0;
 }
